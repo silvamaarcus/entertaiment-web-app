1726c808e99d073350ca1bad4dd88ef72766975d
Marcus Sillion <marcus.costa@sillion.com.br>
2024-10-28 09:56:14 -0300

    refactor: Correcao de tipagem dos parâmetros para rota dinâmica em SectionPage

--- src/app/dashboard/[section]/page.tsx
+++ src/app/dashboard/[section]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SectionHome from "./pages/SectionHome";
+import { GetStaticPropsContext } from "next";
 
 type SectionPageProps = {
   params: { section: string };
@@ -23,4 +24,24 @@ const SectionPage = ({ params }: SectionPageProps) => {
       return <div>Seção não encontrada</div>;
   }
 };
+
+// Função para gerar `params` de forma estática
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { section: "home" } }, { params: { section: "bookmark" } }],
+    fallback: "blocking",
+  };
+}
+
+// Função para passar `params` como props
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const { section } = context.params || {};
+  return {
+    props: {
+      params: { section },
+    },
+    notFound: !section,
+  };
+}
+
 export default SectionPage;

1211b8ff988bf433f8beb36d9b653ed5f99e2bff
Marcus Sillion <marcus.costa@sillion.com.br>
2024-11-21 17:55:40 -0300

    fix: estilo do Input

--- src/components/InputMaster.tsx
+++ src/components/InputMaster.tsx
@@ -8,7 +8,7 @@ export interface InputMasterProps {
 
 const InputMaster = ({ placeholder, type, className }: InputMasterProps) => {
   return (
-    <div className="flex items-center gap-3 pr-2 sm:mt-2">
+    <div className="flex items-center gap-3 px-4 sm:mt-2">
       <SearchIcon className="mt-2 h-8 w-8" />
       <input
         type={type}
